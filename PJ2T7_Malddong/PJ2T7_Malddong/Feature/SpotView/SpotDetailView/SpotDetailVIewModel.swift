095d0418e8b7af7c0cb0fba122f579f9ebce78a0
jaewan0091 <wodhks7868@naver.com>
2023-12-11 23:28:36 +0900

    feature / Spot Model, Spot View, Spot View Model 추가

---
+++ PJ2T7_Malddong/PJ2T7_Malddong/Feature/SpotView/SpotDetailView/SpotDetailVIewModel.swift
@@ -0,0 +1,8 @@
+//
+//  SpotDetailVIewModel.swift
+//  PJ2T7_Malddong
+//
+//  Created by 김재완 on 2023/12/11.
+//
+
+import Foundation
---
+++ PJ2T7_Malddong/PJ2T7_Malddong/Model/Spot.swift
@@ -0,0 +1,8 @@
+//
+//  Spot.swift
+//  PJ2T7_Malddong
+//
+//  Created by 김재완 on 2023/12/11.
+//
+
+import Foundation
