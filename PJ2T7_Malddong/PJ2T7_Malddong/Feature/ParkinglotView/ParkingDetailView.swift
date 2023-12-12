778be77c06894adedf9163b537437b2aba975f88
sooobee <20subi@jbnu.ac.kr>
2023-12-12 15:51:15 +0900

    text 사이즈 조정

--- PJ2T7_Malddong/PJ2T7_Malddong/Feature/ParkinglotView/ParkingDetailView.swift
+++ PJ2T7_Malddong/PJ2T7_Malddong/Feature/ParkinglotView/ParkingDetailView.swift
@@ -27,25 +27,25 @@ struct ParkingDetailView: View {
                     .frame(maxWidth: 320,maxHeight: 180)
                     .background(Color.blue)
                 
-                ParkingInfoSection(title: "\(parking.운영요일)", content: Color.gray, fontSize: 15, alignment: .trailing)
+                ParkingInfoSection(title: "\(parking.operatingDays)", content: Color.gray, fontSize: 15, alignment: .trailing)
                 
-                ParkingInfoSection(title: "\(parking.주차장명)", content: .black, fontSize: 25, alignment: .leading)
-                ParkingInfoSection(title: "\(parking.주차장도로명주소)", content: Color.gray, fontSize: 15, alignment: .leading, bottomPadding: 10)
+                ParkingInfoSection(title: "\(parking.name)", content: .black, fontSize: 20, alignment: .leading)
+                ParkingInfoSection(title: "\(parking.rnAdres)", content: Color.gray, fontSize: 15, alignment: .leading, bottomPadding: 10)
                 
                 HStack(alignment: .center, spacing: 50){
-                    ParkingDetailItem(title: "전화번호", value: "\(parking.연락처)", titleColor: .red, valueColor: .gray, alignment: .leading)
-                    ParkingDetailItem(title: "주차구획수", value: "\(parking.주차구획수)", titleColor: .red, valueColor: .gray, alignment: .trailing)
+                    ParkingDetailItem(title: "전화번호", value: "\(parking.phoneNo)", titleColor: .red, valueColor: .gray, alignment: .leading)
+                    ParkingDetailItem(title: "주차구획수", value: "\(parking.enableNum)", titleColor: .red, valueColor: .gray, alignment: .trailing)
                 }.padding()
                 
                 HStack(alignment: .center, spacing: 50) {
-                    ParkingDetailItem(title: "요금정보", value: "\(parking.요금정보)", titleColor: .red, valueColor: .gray, alignment: .leading)
-                    ParkingDetailItem(title: "관리기관명", value: "\(parking.관리기관명)", titleColor: .red, valueColor: .gray, alignment: .trailing)
+                    ParkingDetailItem(title: "요금정보", value: "\(parking.price)", titleColor: .red, valueColor: .gray, alignment: .leading)
+                    ParkingDetailItem(title: "관리기관명", value: "\(parking.manageName)", titleColor: .red, valueColor: .gray, alignment: .trailing)
                 }
                 
                 HStack(alignment: .center, spacing: 50)
                 {
-                    ParkingDetailItem(title: "구분", value: "\(parking.주차장구분)", titleColor: .red, valueColor: .gray, alignment: .leading)
-                    ParkingDetailItem(title: "유형", value: "\(parking.주차장유형)", titleColor: .red, valueColor: .gray, alignment: .trailing)
+                    ParkingDetailItem(title: "구분", value: "\(parking.isPublic)", titleColor: .red, valueColor: .gray, alignment: .leading)
+                    ParkingDetailItem(title: "유형", value: "\(parking.type)", titleColor: .red, valueColor: .gray, alignment: .trailing)
                 }
                 
             }
@@ -69,6 +69,8 @@ private func ParkingDetailItem(title: String, value: String, titleColor: Color, 
     VStack {
         Text(title)
             .foregroundColor(titleColor)
+            .font(.custom("LINESeedKR-Bd", size: 15))
+        
         Text(value)
             .foregroundColor(valueColor)
             .font(.custom("LINESeedKR-Rg", size: 13))
