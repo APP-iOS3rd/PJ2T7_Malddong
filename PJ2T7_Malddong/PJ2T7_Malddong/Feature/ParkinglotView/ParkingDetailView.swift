009cc31a38d471c2d61df5d5a1bac6976394a92a
sooobee <20subi@jbnu.ac.kr>
2023-12-12 09:35:06 +0900

    디테일뷰 저장

--- PJ2T7_Malddong/PJ2T7_Malddong/Feature/ParkinglotView/ParkingDetailView.swift
+++ PJ2T7_Malddong/PJ2T7_Malddong/Feature/ParkinglotView/ParkingDetailView.swift
@@ -11,10 +11,29 @@ struct ParkingDetailView: View {
     var parking: Parking
     
     var body: some View {
+        VStack {
+            Image("주차장")
+            Image("지도")
+            
+            HStack{
+                Text("\(parking.주차장명)")
+                
+                VStack{
+                    Text("\(parking.주차장도로명주소)")
+                    Text("300M")
+                }
+                
+                Text("연락처")
+                Text("\(parking.연락처)")
+                
+            }
+            
+        }
+        
         Text("Parking Detail: \(parking.주차장명)")
     }
 }
 
-#Preview {
-    ParkingDetailView(parking: <#Parking#>)
-}
+//#Preview {
+//    ParkingDetailView(parking: <#Parking#>)
+//}
