ff91626033411eab3adaa404ab735c83c3412d41
jaewan0091 <wodhks7868@naver.com>
2023-12-13 17:32:54 +0900

    feat : ModelTransitionVIew 추가 , Button 클릭/미클릭시 색상추가, 사진크기 맞추기 수정(미완)

---
+++ PJ2T7_Malddong/PJ2T7_Malddong/Feature/ModelTransition/ModelTransitionView.swift
@@ -0,0 +1,18 @@
+//
+//  ModelTransitionView.swift
+//  PJ2T7_Malddong
+//
+//  Created by 김재완 on 2023/12/13.
+//
+
+import SwiftUI
+
+struct ModelTransitionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ModelTransitionView()
+}
